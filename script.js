5a91aab1589138bc1c865f761cc81c0a2ab9ccd3
junaid1039 <ssmbag103955@gmail.com>
2024-06-21 16:36:40 +0500

    website code

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded'){
+
+let counter=document.getElementById('c1');
+let count=1;
+let interval = setInterval(() => {
+    counter.innerText=count;
+    count++;
+    if(count==750){
+        clearInterval(interval);
+    }
+    
+}, 5);
+}
