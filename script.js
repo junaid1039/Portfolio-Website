62fce44b7ec97ba2e3b540e62641cf5353ab4349
junaid1039 <ssmbag103955@gmail.com>
2024-06-22 09:31:39 -0700

    bugs

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-document.addEventListener('DOMContentLoaded');
+document.addEventListener('DOMContentLoaded', () => {
+    let counter = document.getElementById('c1');
+    let count = 0;
+    let interval = setInterval(() => {
+        counter.innerText = count;
+        count++;
+        if (count == 750) {
+            clearInterval(interval);
+        }
+    }, 5);
 
-{
-let counter=document.getElementById('c1');
-let count=1;
-let interval = setInterval(() => {
-    counter.innerText=count;
-    count++;
-    if(count==750){
-        clearInterval(interval);
+    //header background color changing on scrolling
+    window.onscroll = () => {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        var header = document.getElementById('header');
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            header.style.backgroundColor = "#FFFFFF"; // Change to white when scrolled down
+        } else {
+            header.style.backgroundColor = 'transparent'; // Default color when at the top
+        }
     }
-    
-}, 5);
-}
+
+});
