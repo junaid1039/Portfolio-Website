c7cea9d35cd2634bb85ea73e77aa6697f2c77bf5
junaid1039 <ssmbag103955@gmail.com>
2024-06-30 04:10:35 -0700

    responsive test

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-
-    function show(){
-        var shoo = document.getElementsByID('menu');
-        shoo.innerText='left: 0px'
+// header response javascript code
+    nav = document.querySelector('#bar');
+    nav.onclick = function(){
+      navigations = document.querySelector('.nave');
+      navigations.classList.toggle("active");
     }
-
-    //header background color changing on scrolling
-    window.onscroll = () => {
-        scrollFunction();
-    };
+//header code end here
 
     function scrollFunction() {
         var header = document.getElementsByClassName('header');
@@ -19,23 +15,5 @@ document.addEventListener('DOMContentLoaded', () => {
             header.style.backgroundColor = 'transparent'; // Default color when at the top
         }
     }
-
-    // Function to scroll to section-2 and start counter
-    function sectionto() {
-        let section2 = document.getElementById('c1');
-        section2.scrollIntoView({ behavior: 'smooth' });
-
-        let counter = document.getElementById('c1');
-        let count = 0;
-        let interval = setInterval(() => {
-            counter.innerText = count;
-            count++;
-            if (count == 750) {
-                clearInterval(interval);
-            }
-        }, 5);
-    }
-
-    // Scroll to section-2 when needed
-    
+      
 });
